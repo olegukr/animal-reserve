731655e47e5a71d683f8effcab3b51c2cf29d188
olegukr <oleg.poberezhets@gmail.com>
2025-01-22 20:42:44 +0100

    first commit

---
+++ src/main/java/org/factoriaf5/animal_reserve/AnimalReserveApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.animal_reserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalReserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/factoriaf5/animal_reserve/AnimalReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.animal_reserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
